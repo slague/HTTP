9d95346273b6c254a7fee981595e40829b532c96
slague <stephanielague@gmail.com>
2017-02-12 11:04:53 -0700

    Iteration 1

--- lib/http.rb
+++ lib/http.rb
@@ -4,7 +4,6 @@ tcp_server = TCPServer.new(9292)
 
 puts "Ready for a request"
 
-counter = 1
 
 while client = tcp_server.accept
   request_lines = []
@@ -16,9 +15,37 @@ while client = tcp_server.accept
   puts "Got this request:"
   puts request_lines.inspect
 
+  verb = request_lines[0].split[0]
+  path = request_lines[0].split[1]
+
+  if path == '/favicon.ico'
+    client.puts ["http/1.1 404 not-found"]
+    next
+  end
+
+  protocol = request_lines[0].split[2]
+  host = request_lines[1].split(":")[1].lstrip
+  port = request_lines[1].split(":")[2]
+  origin = host
+  accept = request_lines[-3].split(":")[1].lstrip
+
+header_string = <<END_OF_HEADERS
+  <pre>
+    Verb: #{verb}
+    Path: #{path}
+    Protocol: #{protocol}
+    Host: #{host}
+    Port: #{port}
+    Origin: #{origin}
+    Accept: #{accept}
+  </pre>
+END_OF_HEADERS
+
+  # require "pry"; binding.pry
+
   puts "Sending response."
 
-  response = "<h1>" + "Hello, world! #{counter}\n" + "</h1>"
+  response = header_string
   output = "<html><head></head><body>#{response}</body></html>"
   headers = ["http/1.1 200 ok",
             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
